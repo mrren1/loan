c925990ec3a1e39ecfd590a5f7768b75729bd0d0
yxl <2868929938@qq.com>
2017-08-31 19:33:56 +0800

    moban

--- app/Http/Controllers/backend/PowerController.php
+++ app/Http/Controllers/backend/PowerController.php
@@ -1,9 +1,9 @@
 <?php
 namespace App\Http\Controllers\backend;
-use App\Rbac;
+use App\Power;
 use App\Http\Controllers\fronted\Controller;
 use Illuminate\Support\Facades\DB;
-class RbacController extends Controller
+class PowerController extends Controller
 {
 
      /**
@@ -12,41 +12,41 @@ class RbacController extends Controller
       */
      public function admin_role()
      {
-     	return view('backend/Rbac/admin_role');
+     	return view('backend/Power/admin_role');
      }
      /**
       * 角色添加
       */
      public function admin_role_add()
      {
-     	return view('backend/Rbac/admin_role_add');
+     	return view('backend/Power/admin_role_add');
      }
      /**
       * 权限管理
       */
      public function admin_permission()
      {
-         return view('backend/Rbac/admin_permission');
+         return view('backend/Power/admin_permission');
      }
      /**
       * 权限添加
       */
       public function admin_permission_add()
       {
-      	return view('backend/Rbac/admin_permission_add');
+      	return view('backend/Power/admin_permission_add');
       }
      /**
       * 管理员列表
       */
      public function admin_list()
      {
-     	return view('backend/Rbac/admin_list');
+     	return view('backend/Power/admin_list');
      }
      /**
       * 管理员添加
       */
       public function admin_add()
      {
-     	return view('backend/Rbac/admin_add');
+     	return view('backend/Power/admin_add');
      }
 }
