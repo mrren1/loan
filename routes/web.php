3a3d23b7d42b7313557b240434ab4da82ebc1e2e
jing <1529271269@qq.com>
2017-09-13 11:05:39 +0800

    friends

--- routes/web.php
+++ routes/web.php
@@ -64,7 +64,8 @@ Route::any('fronted_integral', 'fronted\User_sign_inController@fronted_integral'
 
 //好友列表
 Route::any('friend_list','fronted\FriendController@friend_list');
-
+Route::post('friends','fronted\FriendController@friend_sele');
+Route::post('friend_add','fronted\FriendController@friend_add');
 //注册用户
 
 Route::post('register', 
